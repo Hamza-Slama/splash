bd116972eadf07762f2989f549f50e01024195e3
Hamza Slama <hamzaslama8@gmail.com>
2018-07-16 10:31:28 +0100

    initial commit

---
+++ app/src/main/java/com/example/odc/splash/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.odc.splash
+
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
