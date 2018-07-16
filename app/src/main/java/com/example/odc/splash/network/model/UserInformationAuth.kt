2decd077f5961f874033e288ac9fabc545ce559f
Hamza Slama <hamzaslama8@gmail.com>
2018-07-16 11:39:59 +0100

    mvp part

---
+++ app/src/main/java/com/example/odc/splash/network/model/UserInformationAuth.kt
@@ -0,0 +1,2 @@
+package com.example.odc.splash.network.model
+
--- app/src/main/java/com/example/odc/splash/ui/SplashActivity.kt
+++ app/src/main/java/com/example/odc/splash/ui/SplashActivity.kt
@@ -1,6 +1,5 @@
 package com.example.odc.splash
 
-import android.support.v4.content.ContextCompat.startActivity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -11,7 +10,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }
